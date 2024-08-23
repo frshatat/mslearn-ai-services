8462b98abdddedb7d491e305c8c364c33364a477
Faris Shatat <farisshatat4@gmail.com>
2024-08-22 19:40:38 -0700

    remove api keys

--- Labfiles/02-ai-services-security/rest-test.cmd
+++ Labfiles/02-ai-services-security/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "<your-endpoint>/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <your-key>" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
+curl -X POST "https://faristrain-secureai.openai.azure.com/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 99a38c308ae3465684af0a4c963687d7" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
